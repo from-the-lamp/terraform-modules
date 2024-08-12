c61b9d7a65969e9d4abbc9c327fb46c7bcd3387d
djinn <artyom.zverev@proton.me>
2024-08-12 13:05:56 +0300

    Test

--- oracle/kms_vault/main.tf
+++ oracle/kms_vault/main.tf
@@ -8,7 +8,6 @@ resource "oci_kms_key" "template" {
   compartment_id      = var.compartment_ocid
   display_name        = "${var.display_name}-key"
   management_endpoint = oci_kms_vault.template.management_endpoint
-  vault_id            = oci_kms_vault.template.id
   key_shape {
     algorithm = "AES"
     length    = 256
