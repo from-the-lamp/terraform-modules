b872cbb2141e7d9891d65da0b461691a7ecf90a4
djinn <artyom.zverev@proton.me>
2025-08-20 12:14:15 +0300

    Test

--- kubernetes/helm/variables.tf
+++ kubernetes/helm/variables.tf
@@ -1,18 +1,3 @@
-variable "kubernetes_host" {
-  type = string
-}
-
-variable "kubernetes_client_certificate" {
-  type = string
-}
-
-variable "kubernetes_client_key" {
-  type = string
-}
-
-variable "kubernetes_cluster_ca_certificate" {
-  type = string
-}
 variable "helm_release_name" {
   type        = string
   default     = "base"
