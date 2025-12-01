08ccfc61fed95fb99905bd79e20c20d23f6aef61
djinn <artyom.zverev@proton.me>
2025-12-01 14:49:48 +0200

    Test

--- cloudflare/tunnel/output.tf
+++ cloudflare/tunnel/output.tf
@@ -6,7 +6,12 @@ output "cname" {
   value = "${cloudflare_zero_trust_tunnel_cloudflared.template.id}.cfargotunnel.com"
 }
 
-# output "token" {
-#   value     = cloudflare_zero_trust_tunnel_cloudflared.template.tunnel_token
-#   sensitive = true
-# }
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "template" {
+  account_id = var.account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.template.id
+}
+
+output "token" {
+  value     = data.cloudflare_zero_trust_tunnel_cloudflared_token.template.token
+  sensitive = true
+}
